3ba7e40bee0e208a62161e6b8a56b80cce357797
2ffect <dx_jh@naver.com>
2025-02-07 00:33:17 +0900

    0207

--- TIL/Algorithm_Problem_Solving/0206/swea_4834.py
+++ TIL/Algorithm_Problem_Solving/0206/swea_4834.py
@@ -8,7 +8,18 @@ for tc in range(1, T+1):
     C = len(counts)
     for i in range(N):
         counts[arr[i]] += 1
-    
-    # 각 최대 값 정의
-    max_num = 0
+    # print(list)
+
+    # 가장 큰 값을 가진 인덱스 찾기 
     max_idx = 0
+    max_num = 0
+
+    for j in range(C):
+        if counts[j] >= max_num: # 값이 같으면 큰 값 >=
+            max_num = counts[j]
+            max_idx = j
+            
+
+    print(f'#{tc} {max_idx} {max_num}')
+
+
