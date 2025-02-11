37acebcc3cf1ffa7770133b359f602255fd84b98
effect <dx_jh@naver.com>
2025-02-11 11:24:25 +0900

    test.py

--- test.py
+++ test.py
@@ -1,8 +1,7 @@
-from pprint import pprint
+txt = list(input())
+N = len(txt)
 
-### 전치 
-matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-reverse = list(map(list, zip(*matrix)))
+for i in range(N//2):
+    txt[i], txt[N-1-i] = txt[N-1-i], txt[i]
 
-### 90도 시계방향 회전
-turn_90 = list(map(list, zip(*matrix[::-1])))
+print(txt)
