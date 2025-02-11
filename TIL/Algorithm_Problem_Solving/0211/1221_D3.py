9ca9fa3b601f3c0873a8a46cd4ce669f4f35625b
2ffect <dx_jh@naver.com>
2025-02-11 20:14:47 +0900

    0211 problems

---
+++ TIL/Algorithm_Problem_Solving/0211/1221_D3.py
@@ -0,0 +1,24 @@
+import sys
+
+sys.stdin = open("1221_input.txt", "r")
+
+
+T = int(input())
+
+# GNS
+
+for tc in range(1):
+    title = list(map(str, input().split()))
+    arr = list(map(str, input().split()))
+    num = ["ZRO", "ONE", "TWO", "THR", "FOR", "FIV", "SIX", "SVN", "EGT", "NIN"]
+    counts = [0] * 10
+
+    temp = [""]
+
+    arr_len = int(title[1])
+    print(arr_len)
+
+
+    result = ' '.join(new_list)
+
+    print(f'{title[0]} {result}')
--- TIL/Algorithm_Problem_Solving/0211/5356_D3.py
+++ TIL/Algorithm_Problem_Solving/0211/5356_D3.py
@@ -1,22 +1,13 @@
 import sys
 sys.stdin = open("5356_input.txt", "r")
 
+# 의석이 세로로 말해요
+
 T = int(input())
 
 for tc in range(1, T+1):
     arr = [list(map(str, input())) for _ in range(5)]
 
-    for i in range(len(arr)):
-        cnt arr[i]
+    result = ''.join(new_list)
 
-
-    # for j in range(N):
-    #     for i in range(5):
-    #         point = arr[i][j]
-    #
-    #         new_list.append(point)
-    #
-    #
-    # result = ''.join(new_list)
-
-    # print(f'#{tc} {result}')
+    print(f'#{tc} {result}')
