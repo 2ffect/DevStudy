896a9de1ced7fe9c2746723cb591f1bea1434483
2ffect <dx_jh@naver.com>
2025-02-27 21:53:11 +0900

    250227 solve

--- Algorithm_Problem_Solving/250227/BOJ_14503.py
+++ Algorithm_Problem_Solving/250227/BOJ_14503.py
@@ -3,7 +3,7 @@
 # 방의 크기 N * M
 N, M = map(int, input().split())
 # 청소기 초기 위치와 머리 방향
-I, J, D = map(int, input().split())
+i, j, d = map(int, input().split())
 # 방 상태 1 = 벽, 0 = 청소가 되어있지 않은 구역
 room = [list(map(int, input().split())) for _ in range(N)]
 
@@ -15,46 +15,44 @@ dj = [0, 1, 0, -1]
 # 청소 횟수
 clean_cnt = 0
 
-i, j, d = I, J, D
-
-# 청소 할 구역이 없을 때 까지 계속 진행
+# 종료조건 발생 시 까지 반복
 while True:
-    # 청소기의 위치에 청소가 되어있지 않은 경우,
+    # 청소기의 위치가 0 이면 청소 진행 후 횟수 추가
     if room[i][j] == 0:
-        # 청소를 진행한다. 현재 위치 상태를 0에서 -1로 바꾼다.
         room[i][j] = -1
-        # 청소 횟수를 1회 증가 시킨다.
         clean_cnt += 1
-        # 청소 후 머리 돌려주기
+
+
+    # 아니라면, 탐색 반 시계 방향으로
+    for _ in range(4):
         d = (d - 1) % 4
 
-    # 이동 한 구간이 이미 청소 된 경우, 해당 구역에서 재탐색
-    elif room[i][j] == -1:
-        d = (d - 1) % 4
+        # 청소기의 다음 탐색지점 갱신
+        ni = i + di[d]
+        nj = j + dj[d]
 
-    # 탐색 한 좌표
-    ni = i + di[d]
-    nj = j + dj[d]
-    # 유효하면서 청소가 되어있지 않은 경우
-    if (0 <= ni < N) and (0 <= nj < M) and room[ni][nj] == 0:
-        # 청소기를 이동 시킨 후 청소를 하러 간다.
-        i, j = ni, nj
-        continue
 
-    # 유효한데, 청소할 곳이 없는 경우 == 후진을 해야한다.
-    elif (0 <= ni < N) and (0 <= nj < M) and room[ni][nj] != 0:
-        # ni, nj를 후진 한 위치로 새로 정의
-        ni = i - di[d]
-        nj = j - dj[d]
-        # 다시 유효성 검사 진행, 후진한 위치가 벽이 아닐 경우,
-        if (0 <= ni < N) and (0 <= nj < M) and room[ni][nj] != 1:
-            # 후진 위치로 청소기를 이동시킨 후 청소하러 간다.
+        # 다음 위치가 유효하다고 청소가 가능하면
+        if (0 <= ni < N) and (0 <= nj < M) and room[ni][nj] == 0:
+            # 좌표 옮기고 for문 탈출
             i, j = ni, nj
-            continue
-        # 만약 후진한 위치가 벽이라면, 청소를 종료한다.
-        elif (0 <= ni < M) and (0 <= nj < N) and room[ni][nj] == 1:
-            # break for while True
             break
 
-# 청소가 끝났다면 청소 횟수를 출력한다.
+        # 다음 위치가 유효한데 이미 청소가 되어 있다면
+        elif (0 <= ni < N) and (0 <= nj < M) and room[ni][nj] == -1:
+            continue
+
+    # 4 방향 모두 청소가 불가능 하다면
+    else:
+        # 후진
+        ni = i - (di[d])
+        nj = j - (dj[d])
+        # 유효하고 벽이 아니라면
+        if (0 <= ni < N) and (0 <= nj < M) and room[ni][nj] != 1:
+            i, j = ni, nj
+
+        # 벽이라면
+        elif (0 <= ni < N) and (0 <= nj < M) and room[ni][nj] == 1:
+            break
+
 print(clean_cnt)
--- test.py
+++ test.py
@@ -1,4 +1,44 @@
-import datetime
+N, M = map(int, input().split())   #  N*M 방
+i, j, dr = map(int, input().split())  # 현재x좌표 현재y좌표 현재방향
+arr = [list(map(int, input().split())) for _ in range(N)]
 
-d_today = datetime.date.today()
-print(d_today)
+di = [-1, 0, 1, 0]          # 상 좌 하 우 (반시계방향 회전)
+dj = [0, -1, 0, 1]
+count = 0
+
+
+while True:
+    if arr[i][j] == 0:   # 처음은 청소 안된칸
+        arr[i][j] = 2   # 청소했다고 표시해준다
+        count += 1      # 청소 횟수를 올려줌
+        # 이제 90도 회전하면서 찾아야하는데
+    for k in range(4):
+        dr = (dr + 1) % 4  # 반시계 90도 회전해서 방향바꾸고
+
+        ni = i + di[dr]
+        nj = j + dj[dr]
+        # 해당 좌표가 범위를 안나가고 / 청소도 안됐으면
+        if 0 <= ni < N and 0 <= nj < M and arr[ni][nj] == 0:
+            i = ni      # 좌표 갱신 (로청 이동)
+            j = nj
+            # 카운트,2로변경은 안해줘도됨 왜냐면 break해서 위로돌아가서 어차피 할거니깐
+            # 다시 처음으로 돌아간다
+            break
+
+        # 다음 위치가 유효한데 이미 청소가 되어 있다면
+        elif (0 <= ni < N) and (0 <= nj < M) and arr[ni][nj] == -1:
+            continue
+
+    else: # 회전해서 간 칸이 청소 돼있으면
+        # 후진할거임.
+        i = i - di[dr]
+        j = j - dj[dr]
+        # 만약 후진을 못하면 break 할거니깐.
+        # 만약 벽이거나 튀어나가면;
+        if (i >= N or i < 0 or j >= M or j < 0):
+            break
+        if arr[i][j] == 1:
+            # 청소끝.
+            break
+
+print(count)
