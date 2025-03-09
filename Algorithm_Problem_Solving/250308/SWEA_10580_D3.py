9a4d8510769cc38dc4f164060a997f0a9b15b055
2ffect <dx_jh@naver.com>
2025-03-09 19:08:16 +0900

    250309 solve

--- Algorithm_Problem_Solving/250308/SWEA_10580_D3.py
+++ Algorithm_Problem_Solving/250308/SWEA_10580_D3.py
@@ -1,7 +1,7 @@
 # 전봇대
 
-# 시작 시간 : 03/08 13:45
-# 종료 시간 : 03/08 13:57
+# 시작 시간 : 03/09 18:50
+# 종료 시간 : 03/09 19:06
 
 import sys
 sys.stdin = open('10580_input.txt')
@@ -11,28 +11,30 @@ T = int(input())
 for tc in range(1, T+1):
     # 전선의 수
     N = int(input())
+    # 전선 리스트
+    arr = [list(map(int, input().split())) for _ in range(N)]
 
-    # 전봇대 b (a는 필요없다.)
-    b_post = [0] * 10001
-    # 전선이 겹친 횟수
+    # 겹친 전선의 수
     cnt = 0
-    for _ in range(N):
-        # 전선의 위치 인풋 받기
-        a, b = map(int, input().split())
 
-        # a 부터 b 까지 전봇대에 1 로 전선의 영역을 표시
-        for i in range(a, b+1):
-            b_post[i] += 1
+    # 전선을 하나씩 가져와서 기준점으로 삼기
+    for i in range(N):
+        # 기준 전선의 시작점
+        start = arr[i][0]
+        # 기준 전선의 끝점
+        end = arr[i][1]
 
-        else:
-            # 숫자가 2가 된다면 겹친 것 모든 범위가 2라면 겹친 횟수를 1만 올려주기
-            for j in range(a, b+1):
-                # 조회 했을때 2 이상이 아니라면 ? break
-                if b_post[j] < 2:
-                    break
-                # 2 이상이면 2번 겹친것, cnt +1
+        # 기준점 다음 전선부터, 끝 전선까지 비교
+        for j in range(i + 1, N):
+            # 비교대상 전선의 시작점 끝점
+            a, b = arr[j][0], arr[j][1]
 
-            else:
+            # 비교대상 전선의 시작점이 기준 전선보다 낮으면서 끝점이 기준 전선의 끝점보다 높으면 겹친다.
+            if (a < start) and (b > end):
+                cnt += 1
+
+            # 비교대상 전선의 시작점이 기준 전선보다 높으면서 끝점이 기준 전선의 끝점보다 낮으면 겹친다.
+            elif (a > start) and (b < end):
                 cnt += 1
 
     print(f'#{tc} {cnt}')
